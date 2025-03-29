b3f95455e45cd22e4c1f4b1fe0e356dfa8288f34
Insu-Lee <insu.lee@mrhi.or.kr>
2025-03-29 15:33:59 +0900

    env setting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.6',
+      },
+      {
+        version: '0.8.20',
+      },
+    ],
+  },
+  networks: {
+    ganache: {
+      url: 'https://public-en-kairos.node.kaia.io',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+  },
 };
 
 export default config;
